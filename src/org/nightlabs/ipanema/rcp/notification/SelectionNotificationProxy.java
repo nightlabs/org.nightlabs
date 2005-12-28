e8651f0d958a2737adc65791cb178d0a2e016fd5
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2005-12-28 21:56:16 +0000

    method to extract PersistenceCapable out of selection
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jdo@2681 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/ipanema/rcp/notification/SelectionNotificationProxy.java
+++ src/org/nightlabs/ipanema/rcp/notification/SelectionNotificationProxy.java
@@ -11,12 +11,10 @@ import java.util.List;
 import javax.jdo.JDOHelper;
 import javax.jdo.spi.PersistenceCapable;
 
-
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-
 import org.nightlabs.base.notification.SelectionManager;
 import org.nightlabs.notification.NotificationEvent;
 import org.nightlabs.notification.SubjectCarrier;
@@ -42,41 +40,52 @@ public class SelectionNotificationProxy implements ISelectionChangedListener
 		this.zone = zone;
 		this.ignoreInheritance = ignoreInheritance;
 	}
-
+	
 	public void selectionChanged(SelectionChangedEvent event) 
 	{
-    ISelection selection = event.getSelection();
-    if(!selection.isEmpty())
-    {
-    	NotificationEvent e;
-    	if(selection instanceof IStructuredSelection)
-    	{
-    		List subjects = new ArrayList();
-    		Iterator i = ((IStructuredSelection)selection).iterator();
-    		while(i.hasNext())
-    		{
-    			Object o = i.next();
-    			if(o instanceof PersistenceCapable)
-    				subjects.add(JDOHelper.getObjectId(o));
-    			else
-    				subjects.add(o);
-    		}
+		ISelection selection = event.getSelection();
+		if(!selection.isEmpty())
+		{
+			NotificationEvent e;
+			if(selection instanceof IStructuredSelection)
+			{
+				List<Object> subjects = new ArrayList<Object>();
+				Iterator i = ((IStructuredSelection)selection).iterator();
+				while(i.hasNext())
+				{
+					Object o = i.next();
+					PersistenceCapable pc = getPersistenceCapable(o);
+					if (pc != null)
+						subjects.add(JDOHelper.getObjectId(pc));
+					else
+						subjects.add(o);
+				}
 				SubjectCarrier[] subjectCarriers = new SubjectCarrier[subjects.size()];
 				for (int j = 0; j < subjects.size(); j++) {
 					subjectCarriers[j] = new SubjectCarrier(subjects.get(j));
 					subjectCarriers[j].setInheritanceIgnored(ignoreInheritance);
 				}
-        e = new NotificationEvent(source, zone, subjectCarriers);
+				e = new NotificationEvent(source, zone, subjectCarriers);
 				
-//        e = new NotificationEvent(source, zone, subjects.toArray());
-    	}
-    	else { // TODO How can a PersistenceCapable be an ISelection???
-  			if(selection instanceof PersistenceCapable)
-          e = new NotificationEvent(source, zone, JDOHelper.getObjectId(selection));
-  			else
-          e = new NotificationEvent(source, zone, selection);
-    	}
-    	SelectionManager.sharedInstance().notify(e);
-    }
+//				e = new NotificationEvent(source, zone, subjects.toArray());
+			}
+			else { // TODO How can a PersistenceCapable be an ISelection???
+				if(selection instanceof PersistenceCapable)
+					e = new NotificationEvent(source, zone, JDOHelper.getObjectId(selection));
+				else
+					e = new NotificationEvent(source, zone, selection);
+			}
+			SelectionManager.sharedInstance().notify(e);
+		}
+	}
+	
+	/**
+	 * Returns the PersistenceCapable extracted out of the given 
+	 * selectionObject or null if none can be extracted.
+	 */
+	protected PersistenceCapable getPersistenceCapable(Object selectionObject) {
+		if (selectionObject instanceof PersistenceCapable)
+			return (PersistenceCapable)selectionObject;
+		return null;
 	}
 }
