3694f52d17fb2300b67ce19a75a90ddd1dd215c6
maintenance <maintenance@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2005-12-30 14:57:10 +0000

    added/changed license header and license file
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@2860 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/io/IOUtil.java
+++ src/org/nightlabs/base/io/IOUtil.java
@@ -1,10 +1,29 @@
-/**
- * <p> Project: org.nightlabs.base </p>
- * <p> Copyright: Copyright (c) 2005 </p>
- * <p> Company: NightLabs GmbH (Germany) </p>
- * <p> Creation Date: 20.06.2005 </p>
- * <p> Author: Daniel Mazurek </p>
-**/
+/* *****************************************************************************
+ * org.nightlabs.base - NightLabs Eclipse utilities                            *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://www.gnu.org/copyleft/lesser.html                                 *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
 package org.nightlabs.base.io;
 
 import java.io.File;
