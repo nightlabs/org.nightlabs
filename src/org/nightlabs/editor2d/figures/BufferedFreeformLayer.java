6933613b868868e33cff1cfdffd45f1964b58e7a
maintenance <maintenance@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2005-12-30 15:47:52 +0000

    added/changed license header and license file
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.editor2d@2862 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/editor2d/figures/BufferedFreeformLayer.java
+++ src/org/nightlabs/editor2d/figures/BufferedFreeformLayer.java
@@ -1,6 +1,30 @@
-/*
- * Created on 07.06.2005
- */
+/* *****************************************************************************
+ * NightLabs Editor2D - Graphical editor framework                             *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ * Project author: Daniel Mazurek <Daniel.Mazurek [at] nightlabs [dot] org>    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://www.gnu.org/copyleft/lesser.html                                 *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
 package org.nightlabs.editor2d.figures;
 
 import org.eclipse.draw2d.IFigure;
--- src/org/nightlabs/editor2d/handle/EditorAbstractHandle.java
+++ src/org/nightlabs/editor2d/handle/EditorAbstractHandle.java
@@ -1,10 +1,30 @@
-/**
- * <p> Project: org.nightlabs.editor2d </p>
- * <p> Copyright: Copyright (c) 2004 </p>
- * <p> Company: NightLabs GmbH (Germany) </p>
- * <p> Creation Date: 14.04.2005 </p>
- * <p> Author: Daniel Mazurek </p>
-**/
+/* *****************************************************************************
+ * NightLabs Editor2D - Graphical editor framework                             *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ * Project author: Daniel Mazurek <Daniel.Mazurek [at] nightlabs [dot] org>    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://www.gnu.org/copyleft/lesser.html                                 *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+
 package org.nightlabs.editor2d.handle;
 
 import org.eclipse.draw2d.Locator;
