39b62c49266f287f3325b33dbb37f923289c78a0
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-01-24 18:22:20 +0000

    based on forms
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.editor2d@3253 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/editor2d/composite/QuickOptionsComposite.java
+++ src/org/nightlabs/editor2d/composite/QuickOptionsComposite.java
@@ -40,10 +40,19 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Layout;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.eclipse.ui.forms.widgets.Section;
+import org.eclipse.ui.forms.widgets.TableWrapData;
+import org.eclipse.ui.forms.widgets.TableWrapLayout;
 import org.nightlabs.base.composite.XComposite;
 import org.nightlabs.base.composite.XComposite.LayoutMode;
+import org.nightlabs.base.form.XFormToolkit;
 import org.nightlabs.config.Config;
 import org.nightlabs.config.ConfigException;
 import org.nightlabs.editor2d.EditorPlugin;
@@ -55,7 +64,7 @@ import org.nightlabs.editor2d.config.QuickOptionsConfigModule;
 public class QuickOptionsComposite 
 extends XComposite 
 {
-
+	
 	/**
 	 * @param parent
 	 * @param style
@@ -82,7 +91,19 @@ extends XComposite
 	protected void init() 
 	{
 		initConfigModule();
+		initLayout();
 		createComposite(this);
+	}
+		
+	protected void initLayout() 
+	{
+		GridLayout layout = new GridLayout();
+		layout.marginBottom = 0;
+		layout.marginTop = 0;
+		layout.marginLeft = 0;
+		layout.marginRight = 0;
+		setLayout(layout);
+		setLayoutData(new GridData(GridData.FILL_BOTH));
 	}
 	
 	protected QuickOptionsConfigModule confMod = null;
@@ -103,21 +124,69 @@ extends XComposite
 		} 
 	}
 	
+//	protected void createComposite(Composite parent) 
+//	{
+//		Group cloneGroup = new Group(parent, SWT.NONE);
+//		cloneGroup.setText(EditorPlugin.getResourceString("quickOptionsView.group.clone.name"));
+//		cloneGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+//		cloneGroup.setLayout(new GridLayout());
+//		createEntry(cloneGroup, EditorPlugin.getResourceString("quickOptions.distanceCloneX.label"), CLONE_X);
+//		createEntry(cloneGroup, EditorPlugin.getResourceString("quickOptions.distanceCloneY.label"), CLONE_Y);
+//		
+//		Group moveGroup = new Group(parent, SWT.NONE);
+//		moveGroup.setText(EditorPlugin.getResourceString("quickOptionsView.group.move.name"));
+//		moveGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+//		moveGroup.setLayout(new GridLayout());
+//		createEntry(moveGroup, EditorPlugin.getResourceString("quickOptions.distanceMoveX.label"), MOVE_X);
+//		createEntry(moveGroup, EditorPlugin.getResourceString("quickOptions.distanceMoveY.label"), MOVE_Y);
+//	}
+	private FormToolkit toolkit = null;
+	private ScrolledForm form = null;	
 	protected void createComposite(Composite parent) 
 	{
-		Group cloneGroup = new Group(parent, SWT.NONE);
-		cloneGroup.setText(EditorPlugin.getResourceString("quickOptionsView.group.clone.name"));
-		cloneGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		cloneGroup.setLayout(new GridLayout());
-		createEntry(cloneGroup, EditorPlugin.getResourceString("quickOptions.distanceCloneX.label"), CLONE_X);
-		createEntry(cloneGroup, EditorPlugin.getResourceString("quickOptions.distanceCloneY.label"), CLONE_Y);
+		toolkit = new XFormToolkit(parent.getDisplay());
+		form = toolkit.createScrolledForm(parent);
+		TableWrapLayout layout = new TableWrapLayout();
+		form.getBody().setLayout(layout);		
+		form.getBody().setLayoutData(new GridData(GridData.FILL_BOTH));
+				
+		Section sectionDuplicate = toolkit.createSection(form.getBody(), 
+			  Section.DESCRIPTION|Section.TITLE_BAR|
+			  Section.TWISTIE|Section.EXPANDED);
+		TableWrapData td = new TableWrapData(TableWrapData.FILL_GRAB);
+		sectionDuplicate.setLayoutData(td);		
 		
-		Group moveGroup = new Group(parent, SWT.NONE);
-		moveGroup.setText(EditorPlugin.getResourceString("quickOptionsView.group.move.name"));
-		moveGroup.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		moveGroup.setLayout(new GridLayout());
-		createEntry(moveGroup, EditorPlugin.getResourceString("quickOptions.distanceMoveX.label"), MOVE_X);
-		createEntry(moveGroup, EditorPlugin.getResourceString("quickOptions.distanceMoveY.label"), MOVE_Y);
+		sectionDuplicate.addExpansionListener(new ExpansionAdapter() {
+			public void expansionStateChanged(ExpansionEvent e) {
+				form.reflow(true);
+		  }
+		});		
+		sectionDuplicate.setText(EditorPlugin.getResourceString("quickOptions.section.clone.text"));
+		sectionDuplicate.setDescription(EditorPlugin.getResourceString("quickOptions.section.clone.description"));
+		Composite sectionClientDuplicate = toolkit.createComposite(sectionDuplicate);	
+		sectionClientDuplicate.setLayout(new GridLayout(2, false));				
+		createEntry(sectionClientDuplicate, EditorPlugin.getResourceString("quickOptions.distanceCloneX.label"), CLONE_X);
+		createEntry(sectionClientDuplicate, EditorPlugin.getResourceString("quickOptions.distanceCloneY.label"), CLONE_Y);
+		sectionDuplicate.setClient(sectionClientDuplicate);
+		
+		Section sectionMove = toolkit.createSection(form.getBody(), 
+			  Section.DESCRIPTION|Section.TITLE_BAR|
+			  Section.TWISTIE|Section.EXPANDED);
+		TableWrapData tdMove = new TableWrapData(TableWrapData.FILL_GRAB);
+		sectionMove.setLayoutData(tdMove);
+		
+		sectionMove.addExpansionListener(new ExpansionAdapter() {
+			public void expansionStateChanged(ExpansionEvent e) {
+				form.reflow(true);
+		  }
+		});		
+		sectionMove.setText(EditorPlugin.getResourceString("quickOptions.section.move.text"));
+		sectionMove.setDescription(EditorPlugin.getResourceString("quickOptions.section.move.description"));
+		Composite sectionClientMove = toolkit.createComposite(sectionMove);
+		sectionClientMove.setLayout(new GridLayout(2, false));				
+		createEntry(sectionClientMove, EditorPlugin.getResourceString("quickOptions.distanceMoveX.label"), MOVE_X);
+		createEntry(sectionClientMove, EditorPlugin.getResourceString("quickOptions.distanceMoveY.label"), MOVE_Y);
+		sectionMove.setClient(sectionClientMove);
 	}
 	
 	protected static final int CLONE_X = 1; 
@@ -127,25 +196,40 @@ extends XComposite
 	
 	protected Map<Spinner, Integer> spinner2Identifier = new HashMap<Spinner, Integer>();
 	
+//	protected void createEntry(Composite parent, String labelText, int identifier) 
+//	{
+//		XComposite comp = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
+//		GridLayout layout = new GridLayout(2, true);
+//		comp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+//		
+//		Label l = new Label(comp, SWT.NONE);
+//		l.setText(labelText);
+//		Spinner spinner = new Spinner(comp, SWT.BORDER);
+//		spinner.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));		
+//		
+//		spinner.setSelection(getValue(identifier));		
+//		
+//		spinner.addSelectionListener(textListener);
+//		spinner.addFocusListener(focusListener);
+//		spinner.addDisposeListener(disposeListener);
+//		
+//		spinner2Identifier.put(spinner, new Integer(identifier));
+//	}
 	protected void createEntry(Composite parent, String labelText, int identifier) 
 	{
-		XComposite comp = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
-		GridLayout layout = new GridLayout(2, true);
-		comp.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		toolkit.paintBordersFor(parent);
 		
-		Label l = new Label(comp, SWT.NONE);
-		l.setText(labelText);
-		Spinner spinner = new Spinner(comp, SWT.BORDER);
+		Label l = toolkit.createLabel(parent, labelText);		
+		Spinner spinner = new Spinner(parent, SWT.NONE);
+		spinner.setData(FormToolkit.KEY_DRAW_BORDER, FormToolkit.TEXT_BORDER);
 		spinner.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));		
-		
-		spinner.setSelection(getValue(identifier));		
-		
+		spinner.setSelection(getValue(identifier));				
 		spinner.addSelectionListener(textListener);
 		spinner.addFocusListener(focusListener);
 		spinner.addDisposeListener(disposeListener);
 		
-		spinner2Identifier.put(spinner, new Integer(identifier));
-	}
+		spinner2Identifier.put(spinner, new Integer(identifier));		
+	}	
 	
 	protected SelectionListener textListener = new SelectionListener()
 	{	
