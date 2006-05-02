bcdf5bb04f81b7b0196917e85436765ea6e2549f
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-05-02 17:54:32 +0000

    - extended SelectionNotificationProxy (can clear its selection when an empty selection occurs) - UserMappings for Ipanema1 can now be edited via the JFire RCP Client
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jdo@3772 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jdo/notification/SelectionNotificationProxy.java
+++ src/org/nightlabs/jdo/notification/SelectionNotificationProxy.java
@@ -27,8 +27,12 @@
 package org.nightlabs.jdo.notification;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import javax.jdo.JDOHelper;
 import javax.jdo.spi.PersistenceCapable;
@@ -50,24 +54,45 @@ public class SelectionNotificationProxy implements ISelectionChangedListener
 	protected Object source;
 	protected String zone;
 	protected boolean ignoreInheritance;
-	
-	public SelectionNotificationProxy(Object source)
-	{
-		this(source, null, false);
-	}
-	
-	public SelectionNotificationProxy(Object source, String zone, boolean ignoreInheritance)
+	protected boolean clearOnEmptySelection;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param source Which will be the source of all notifications?
+	 * @param zone The zone in which selections occur.
+	 * @param ignoreInheritance If <code>true</code>, all classes will be managed separately as if inheritance doesn't exist.
+	 *		If <code>false</code>, the superclasses (recursively) of the selected objects will be assigned with the new selection. 
+	 * @param clearOnEmptySelection If <code>true</code>, an instance will track all classes that have been selected
+	 *		and in case of an empty selection, it will clear all selections that are assigned to these collected classes.
+	 *		If <code>false</code>, empty selections are ignored and do not have any effect (do not modify the state of assigned selections).
+	 */
+	public SelectionNotificationProxy(
+			Object source,
+			String zone,
+			boolean ignoreInheritance,
+			boolean clearOnEmptySelection)
 	{
 		this.source = source;
 		this.zone = zone;
 		this.ignoreInheritance = ignoreInheritance;
+		this.clearOnEmptySelection = clearOnEmptySelection;
 	}
-	
+
+	private Set<Class> managedClasses = new HashSet<Class>();
+
+	private static final List EMPTY_LIST = Collections.unmodifiableList(new LinkedList());
+
 	public void selectionChanged(SelectionChangedEvent event) 
 	{
 		ISelection selection = event.getSelection();
-		if(!selection.isEmpty())
-		{
+		if(selection.isEmpty()) {
+			// we clean the selection for our zone and for all classes that we have learned about so far
+			if (clearOnEmptySelection && !managedClasses.isEmpty())
+				SelectionManager.sharedInstance().notify(
+						new NotificationEvent(source, zone, EMPTY_LIST, managedClasses));	
+		}
+		else {
 			NotificationEvent e;
 			if(selection instanceof IStructuredSelection)
 			{
@@ -76,9 +101,18 @@ public class SelectionNotificationProxy implements ISelectionChangedListener
 				while(i.hasNext())
 				{
 					Object o = i.next();
+					if (clearOnEmptySelection &&
+							o != null && !managedClasses.contains(o.getClass()))
+						managedClasses.add(o.getClass());
+
 					PersistenceCapable pc = getPersistenceCapable(o);
-					if (pc != null)
-						subjects.add(JDOHelper.getObjectId(pc));
+					if (pc != null) {
+						Object oid = JDOHelper.getObjectId(pc);
+						if (clearOnEmptySelection &&
+								oid != null && !managedClasses.contains(oid.getClass()))
+							managedClasses.add(oid.getClass());
+						subjects.add(oid);
+					}
 					else
 						subjects.add(o);
 				}
@@ -88,7 +122,7 @@ public class SelectionNotificationProxy implements ISelectionChangedListener
 					subjectCarriers[j].setInheritanceIgnored(ignoreInheritance);
 				}
 				e = new NotificationEvent(source, zone, subjectCarriers);
-				
+
 //				e = new NotificationEvent(source, zone, subjects.toArray());
 			}
 			else { // TODO How can a PersistenceCapable be an ISelection???
@@ -100,7 +134,7 @@ public class SelectionNotificationProxy implements ISelectionChangedListener
 			SelectionManager.sharedInstance().notify(e);
 		}
 	}
-	
+
 	/**
 	 * Returns the PersistenceCapable extracted out of the given 
 	 * selectionObject or null if none can be extracted.
