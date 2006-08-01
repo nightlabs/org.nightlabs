8a5ea63b1a32cf04f81664dc3870c4f3f22eb93c
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-08-01 16:51:27 +0000

    moved many Methods to org.nightlabs.editor2d.util.GeomUtil (NightLabsEditor2D)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@4278 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/util/GeomUtil.java
+++ src/org/nightlabs/base/util/GeomUtil.java
@@ -68,128 +68,128 @@ public class GeomUtil
 		return new Rectangle(r.x, r.y, r.width, r.height);
 	}	
 	
-	/**
-	 * checks if the given source rectangle is contained in the target Rectangle,
-	 * if the source rectangle is outside of the target Rectangle, 
-	 * the returned rectangle is trimmed so that it fits in the target rectangle
-	 * 
-	 * @param source the source rectangle to check 
-	 * @param target the target rectangle to check if the source rectangle fits in
-	 * @return the a new modified rectangle that it fits in the target rectangle
-	 */
-	public static java.awt.Rectangle checkBounds(java.awt.Rectangle source, java.awt.Rectangle target) 
-	{
-		if (target.contains(source))
-			return new java.awt.Rectangle(target);
-		else 
-		{
-			java.awt.Rectangle trimmedSource = new java.awt.Rectangle(source); 			
-			// is source outter target left
-			if (trimmedSource.x < target.x) {
-				trimmedSource.x = target.x;
-				if (trimmedSource.width > target.width)
-					trimmedSource.width = target.width;
-			}
-			// is source outter target top
-			if (trimmedSource.y < target.y) {
-				trimmedSource.y = target.y;
-				if (trimmedSource.height > target.height)
-					trimmedSource.height = target.height;
-			}
-			// is source outter target right
-			if (trimmedSource.getMaxX() > target.getMaxX()) {
-				trimmedSource.x = (int)target.getMaxX() - trimmedSource.width;
-				if (trimmedSource.width > target.width)
-					trimmedSource.width = target.width;
-			}
-			// is source outter target bottom
-			if (trimmedSource.getMaxY() > target.getMaxY()) {
-				trimmedSource.y = (int) target.getMaxY() - trimmedSource.height;
-				if (trimmedSource.height > target.height)
-					trimmedSource.height = target.height; 
-			}
-			return trimmedSource;
-		}			
-	}
-		
-	/**
-	 * @param rect the Rectangle to translate to Origin (0/0) and adjust the size
-	 * @return a new Rectangle with the origin at (0/0) and newWidth = (oldWidth + oldX), 
-	 * newHeight = (oldHeight + oldY), which means that the size of the returned rectangle 
-	 * is always greater than before, if the oldOrigin wasn't already at (0/0)
-	 */
-	public static java.awt.Rectangle translateToOriginAndAdjustSize(java.awt.Rectangle rect) 
-	{
-		java.awt.Rectangle r = new java.awt.Rectangle(rect);
-		if (r.x < 0) {
-			r.width = r.width - r.x;
-			r.x = 0;
-		}
-		if (r.x > 0) {
-			r.width = r.width + r.x;
-			r.x = 0;
-		}
-		if (r.y < 0) {
-			r.height = r.height - r.y;
-			r.y = 0;
-		}
-		if (r.y > 0) {
-			r.height = r.height + r.y;
-			r.y = 0;
-		}
-		return r;
-	}		
-	
-	/**
-	 * calculates the scaleFactor for width and height by r2 / r1 and returns it
-	 * as Point2D(scaleX, scaleY)
-	 * 
-	 * @param r1 Rectangle r1
-	 * @param r2 Rectangle r2 
-	 * @return the scaleFactors scaleX and scaleY as Point2D 
-	 */
-	public static Point2D calcScale(java.awt.Rectangle r1, java.awt.Rectangle r2) 
-	{
-		double scaleX = 1.0;
-		double scaleY = 1.0;
-		if (r1.width != 0 && r2.width != 0)
-			scaleX = (double)r2.width / (double)r1.width;
-		if (r1.height != 0 && r2.height != 0)
-			scaleY = (double)r2.height / (double)r1.height;
-		
-		return new Point2D.Double(scaleX, scaleY);
-	}	
-	
-	/**
-	 * 
-	 * @param r the Rectangle to translate to Origin (0/0)
-	 * @return a new translated rectangle
-	 */
-	public static java.awt.Rectangle translateToOrigin(java.awt.Rectangle r) 
-	{
-		java.awt.Rectangle newRect = new java.awt.Rectangle(r);
-		newRect.x = 0;
-		newRect.y = 0;
-		return newRect;
-	}			
-	
-	/**
-	 * checks if the dimension of target rectangle is contained in the source rectangle,
-	 * if the dimension target rectangle is greater it gets the dimension of the source rectangle 
-	 * 
-	 * @param source the source rectangle
-	 * @param target the target rectangle to check 
-	 * @return the (if necessary) modified target rectangle
-	 */
-	public static java.awt.Rectangle checkDimension(java.awt.Rectangle source, java.awt.Rectangle target) 
-	{
-		if (target.width > source.width)
-			target.width = source.width;
-		
-		if (target.height > source.height)
-			target.height = source.height;
-		
-		return target;
-	}
+//	/**
+//	 * checks if the given source rectangle is contained in the target Rectangle,
+//	 * if the source rectangle is outside of the target Rectangle, 
+//	 * the returned rectangle is trimmed so that it fits in the target rectangle
+//	 * 
+//	 * @param source the source rectangle to check 
+//	 * @param target the target rectangle to check if the source rectangle fits in
+//	 * @return the a new modified rectangle that it fits in the target rectangle
+//	 */
+//	public static java.awt.Rectangle checkBounds(java.awt.Rectangle source, java.awt.Rectangle target) 
+//	{
+//		if (target.contains(source))
+//			return new java.awt.Rectangle(target);
+//		else 
+//		{
+//			java.awt.Rectangle trimmedSource = new java.awt.Rectangle(source); 			
+//			// is source outter target left
+//			if (trimmedSource.x < target.x) {
+//				trimmedSource.x = target.x;
+//				if (trimmedSource.width > target.width)
+//					trimmedSource.width = target.width;
+//			}
+//			// is source outter target top
+//			if (trimmedSource.y < target.y) {
+//				trimmedSource.y = target.y;
+//				if (trimmedSource.height > target.height)
+//					trimmedSource.height = target.height;
+//			}
+//			// is source outter target right
+//			if (trimmedSource.getMaxX() > target.getMaxX()) {
+//				trimmedSource.x = (int)target.getMaxX() - trimmedSource.width;
+//				if (trimmedSource.width > target.width)
+//					trimmedSource.width = target.width;
+//			}
+//			// is source outter target bottom
+//			if (trimmedSource.getMaxY() > target.getMaxY()) {
+//				trimmedSource.y = (int) target.getMaxY() - trimmedSource.height;
+//				if (trimmedSource.height > target.height)
+//					trimmedSource.height = target.height; 
+//			}
+//			return trimmedSource;
+//		}			
+//	}
+//		
+//	/**
+//	 * @param rect the Rectangle to translate to Origin (0/0) and adjust the size
+//	 * @return a new Rectangle with the origin at (0/0) and newWidth = (oldWidth + oldX), 
+//	 * newHeight = (oldHeight + oldY), which means that the size of the returned rectangle 
+//	 * is always greater than before, if the oldOrigin wasn't already at (0/0)
+//	 */
+//	public static java.awt.Rectangle translateToOriginAndAdjustSize(java.awt.Rectangle rect) 
+//	{
+//		java.awt.Rectangle r = new java.awt.Rectangle(rect);
+//		if (r.x < 0) {
+//			r.width = r.width - r.x;
+//			r.x = 0;
+//		}
+//		if (r.x > 0) {
+//			r.width = r.width + r.x;
+//			r.x = 0;
+//		}
+//		if (r.y < 0) {
+//			r.height = r.height - r.y;
+//			r.y = 0;
+//		}
+//		if (r.y > 0) {
+//			r.height = r.height + r.y;
+//			r.y = 0;
+//		}
+//		return r;
+//	}		
+//	
+//	/**
+//	 * calculates the scaleFactor for width and height by r2 / r1 and returns it
+//	 * as Point2D(scaleX, scaleY)
+//	 * 
+//	 * @param r1 Rectangle r1
+//	 * @param r2 Rectangle r2 
+//	 * @return the scaleFactors scaleX and scaleY as Point2D 
+//	 */
+//	public static Point2D calcScale(java.awt.Rectangle r1, java.awt.Rectangle r2) 
+//	{
+//		double scaleX = 1.0;
+//		double scaleY = 1.0;
+//		if (r1.width != 0 && r2.width != 0)
+//			scaleX = (double)r2.width / (double)r1.width;
+//		if (r1.height != 0 && r2.height != 0)
+//			scaleY = (double)r2.height / (double)r1.height;
+//		
+//		return new Point2D.Double(scaleX, scaleY);
+//	}	
+//	
+//	/**
+//	 * 
+//	 * @param r the Rectangle to translate to Origin (0/0)
+//	 * @return a new translated rectangle
+//	 */
+//	public static java.awt.Rectangle translateToOrigin(java.awt.Rectangle r) 
+//	{
+//		java.awt.Rectangle newRect = new java.awt.Rectangle(r);
+//		newRect.x = 0;
+//		newRect.y = 0;
+//		return newRect;
+//	}			
+//	
+//	/**
+//	 * checks if the dimension of target rectangle is contained in the source rectangle,
+//	 * if the dimension target rectangle is greater it gets the dimension of the source rectangle 
+//	 * 
+//	 * @param source the source rectangle
+//	 * @param target the target rectangle to check 
+//	 * @return the (if necessary) modified target rectangle
+//	 */
+//	public static java.awt.Rectangle checkDimension(java.awt.Rectangle source, java.awt.Rectangle target) 
+//	{
+//		if (target.width > source.width)
+//			target.width = source.width;
+//		
+//		if (target.height > source.height)
+//			target.height = source.height;
+//		
+//		return target;
+//	}
 	
 }
