8d124b8251568589469860d5e43214eacffff146
marc <marc@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-08-12 16:17:15 +0000

    initial
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@4360 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/base/editor/CommitableFormEditor.java
@@ -0,0 +1,83 @@
+/* *****************************************************************************
+ * org.nightlabs.base - NightLabs Eclipse utilities                            *
+ * Copyright (C) 2004-2006 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://www.gnu.org/copyleft/lesser.html                                 *
+ ******************************************************************************/
+package org.nightlabs.base.editor;
+
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.forms.IManagedForm;
+import org.eclipse.ui.forms.editor.FormEditor;
+import org.eclipse.ui.forms.editor.IFormPage;
+
+/**
+ * An abstract form editor that allows commiting
+ * its form pages.
+ * 
+ * @author Marc Klinger - marc[at]nightlabs[dot]de
+ */
+public abstract class CommitableFormEditor extends FormEditor
+{
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * This implementation does not save anything
+	 * but commits all form pages in this editor.
+	 */
+	@Override
+	public void doSave(IProgressMonitor monitor)
+	{
+		commitFormPages(true);
+	}
+
+	/**
+	 * Call commit on all managed from pages.
+	 * @param onSave <code>true</code> to indicate that
+	 * 		we are commiting because of an editor save.
+	 */
+	protected void commitFormPages(boolean onSave) 
+	{
+		IFormPage[] pages = getFormPages();
+		for (int i = 0; i < pages.length; i++) {
+			IFormPage page = pages[i];
+			IManagedForm mform = page.getManagedForm();
+			if (mform != null && mform.isDirty())
+				mform.commit(true);
+		}
+	}
+
+	/**
+	 * Get all form pages in this editor.
+	 * @return All form pages in this editor.
+	 */
+	protected IFormPage[] getFormPages() 
+	{
+		ArrayList<IFormPage> formPages = new ArrayList<IFormPage>(pages.size());
+		for (int i = 0; i < pages.size(); i++) {
+			Object page = pages.get(i);
+			if (page instanceof IFormPage)
+				formPages.add((IFormPage)page);
+		}
+		return formPages.toArray(new IFormPage[formPages.size()]);
+	}
+}
