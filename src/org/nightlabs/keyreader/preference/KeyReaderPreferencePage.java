eb919bb56b1efd11219bcfbe31d99e5a05e34cb4
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-11-09 19:24:33 +0000

    started PreferencePage
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.keyreader@4939 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/keyreader/preference/KeyReaderPreferencePage.java
@@ -0,0 +1,27 @@
+package org.nightlabs.keyreader.preference;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class KeyReaderPreferencePage
+		extends PreferencePage
+		implements IWorkbenchPreferencePage
+{
+
+	@Override
+	protected Control createContents(Composite parent)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void init(IWorkbench workbench)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
