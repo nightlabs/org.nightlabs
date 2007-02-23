56a0e6ef463c30c871ed2966314dc04c5f14254c
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-02-23 19:27:30 +0000

    MessageSectionPart
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@5490 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/base/editor/MessageSectionPart.java
@@ -0,0 +1,72 @@
+/* *****************************************************************************
+ * org.nightlabs.base - NightLabs Eclipse utilities                            *
+ * Copyright (C) 2004-2005 NightLabs - http://NightLabs.org                    *
+ *                                                                             *
+ * This library is free software; you can redistribute it and/or               *
+ * modify it under the terms of the GNU Lesser General Public                  *
+ * License as published by the Free Software Foundation; either                *
+ * version 2.1 of the License, or (at your option) any later version.          *
+ *                                                                             *
+ * This library is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           *
+ * Lesser General Public License for more details.                             *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public            *
+ * License along with this library; if not, write to the                       *
+ *     Free Software Foundation, Inc.,                                         *
+ *     51 Franklin St, Fifth Floor,                                            *
+ *     Boston, MA  02110-1301  USA                                             *
+ *                                                                             *
+ * Or get it online :                                                          *
+ *     http://www.gnu.org/copyleft/lesser.html                                 *
+ *                                                                             *
+ *                                                                             *
+ ******************************************************************************/
+package org.nightlabs.base.editor;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.forms.editor.IFormPage;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+import org.nightlabs.base.entity.editor.EntityEditorUtil;
+
+/**
+ * @author Daniel.Mazurek [at] NightLabs [dot] de
+ *
+ */
+public class MessageSectionPart 
+extends RestorableSectionPart 
+{
+
+	public MessageSectionPart(IFormPage page, Composite parent, int style, String title) {
+		super(parent, page.getEditor().getToolkit(), style);
+		FormToolkit toolkit = page.getEditor().getToolkit();
+		Section section = getSection();
+		section.setText(title);
+		section.setExpanded(true);
+		section.setLayout(new GridLayout());
+		section.setLayoutData(new GridData(GridData.FILL_BOTH));
+		container = EntityEditorUtil.createCompositeClient(toolkit, section, 1);
+		((GridLayout)container.getLayout()).numColumns = 1;		
+		container.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		messageLabel = new Label(container, SWT.NONE);
+		messageLabel.setText(" ");
+		messageLabel.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+	}
+
+	private Composite container;
+	public Composite getContainer() {
+		return container;
+	}
+	
+	private Label messageLabel;
+	public void setMessage(String message) {
+		messageLabel.setText(message);
+	}	
+}
