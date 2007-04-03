34705373877cbc910cc0d7ff61f063c532387959
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-04-03 10:49:51 +0000

    SearchContributionItem nows uses ToolItem and not a Combo
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@6003 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/search/SearchContributionItem.java
+++ src/org/nightlabs/base/search/SearchContributionItem.java
@@ -29,16 +29,26 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.CoolBar;
+import org.eclipse.swt.widgets.CoolItem;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
 import org.nightlabs.base.NLBasePlugin;
 import org.nightlabs.base.action.AbstractContributionItem;
 import org.nightlabs.base.composite.XComposite;
@@ -47,6 +57,7 @@ import org.nightlabs.base.notification.SelectionManager;
 import org.nightlabs.base.resource.SharedImages;
 import org.nightlabs.base.resource.SharedImages.ImageDimension;
 import org.nightlabs.base.resource.SharedImages.ImageFormat;
+import org.nightlabs.base.util.RCPUtil;
 import org.nightlabs.notification.NotificationEvent;
 
 /**
@@ -56,11 +67,13 @@ import org.nightlabs.notification.NotificationEvent;
 public class SearchContributionItem 
 extends AbstractContributionItem 
 {
-
+	private static final Logger logger = Logger.getLogger(SearchContributionItem.class);
+	
 	@Override
-	protected Control createControl(Composite parent) {
+	protected Control createControl(Composite parent) 
+	{
 		Composite comp = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER);
-		GridLayout layout = new GridLayout(3, false);
+		GridLayout layout = new GridLayout(1, false);
 		layout.verticalSpacing = 0;
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
@@ -69,45 +82,14 @@ extends AbstractContributionItem
 		layout.marginTop = 0;
 		layout.marginBottom = 0;
 		comp.setLayout(layout);
+
+		createText(comp);
 		
-		searchText = new Text(comp, SWT.BORDER);
-//		searchText.setSize(150, SWT.DEFAULT);
-		GridData textData = new GridData(150, 15);
-		textData.minimumWidth = 100;		
-		searchText.setLayoutData(textData);
-				
 		searchButton = new Button(comp, SWT.BOTTOM | SWT.PUSH);
-//		Point buttonSize = searchButton.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-//		GridData buttonData = new GridData(buttonSize.x, buttonSize.y);
-//		buttonData.minimumWidth = buttonSize.x;
-//		searchButton.setLayoutData(buttonData);
-		
-//		searchButton.setText(TradePlugin.getResourceString("ProductTypeSearchContributionItem.searchButton.label"));
 		searchButton.setImage(SharedImages.getSharedImage(NLBasePlugin.getDefault(), 
 				SearchContributionItem.class, "", ImageDimension._12x12, ImageFormat.png));		
-		searchText.addSelectionListener(buttonSelectionListener);
 		searchButton.addSelectionListener(buttonSelectionListener);
-		
-//		Menu menu = new Menu(searchButton);
-//		menu.setVisible(true);
-//		menu.setEnabled(true);
-//		searchTypes = new ArrayList<String>(SearchResultProviderRegistry.sharedInstance().getRegisteredNames());
-//		for (String type : searchTypes) {
-//			MenuItem menuItem = new MenuItem(menu, SWT.PUSH);
-//			menuItem.setText(type);
-//			menuItem.addSelectionListener(new SelectionListener(){			
-//				public void widgetSelected(SelectionEvent e) {
-//					selectedType = ((MenuItem) e.getSource()).getText();
-//					searchPressed();
-//				}			
-//				public void widgetDefaultSelected(SelectionEvent e) {
-//					widgetSelected(e);
-//				}			
-//			});
-//		}
-//		searchButton.setMenu(menu);
-//		selectedType = searchTypes.get(0);
-		
+				
 		searchTypeCombo = new Combo(comp, SWT.NONE);
 		fillSearchTypeCombo();
 		searchTypeCombo.addSelectionListener(comboSelectionListener);
@@ -115,10 +97,19 @@ extends AbstractContributionItem
 		return comp;
 	}
 
+	protected void createText(Composite parent) 
+	{
+		searchText = new Text(parent, SWT.BORDER);
+//		GridData textData = new GridData(100, 15);
+//		textData.minimumWidth = 100;
+//		searchText.setLayoutData(textData);		
+		searchText.addSelectionListener(buttonSelectionListener);		
+	}
+	
 	private Text searchText;
-	private Button searchButton;	
 	private List<String> searchTypes = null;
 	
+	private Button searchButton;		
 	private Combo searchTypeCombo;
 	private void fillSearchTypeCombo() 
 	{
@@ -127,22 +118,20 @@ extends AbstractContributionItem
 		if (searchTypeCombo.getItemCount() > 0)
 			searchTypeCombo.select(0);
 	}	
+	
 	private SelectionListener comboSelectionListener = new SelectionListener(){	
 		public void widgetSelected(SelectionEvent e) {
-			 
+			selectedType = searchTypes.get(searchTypeCombo.getSelectionIndex());
 		}	
 		public void widgetDefaultSelected(SelectionEvent e) {
 			widgetSelected(e);
 		}	
-	};	
+	};
+		
+	private String selectedType = null;
 	protected String getSelectedType() {
-		return searchTypes.get(searchTypeCombo.getSelectionIndex());
+		return selectedType;
 	}
-	
-//	private String selectedType = null;
-//	protected String getSelectedType() {
-//		return selectedType;
-//	}
 		
 	private void searchPressed() 
 	{
@@ -172,4 +161,103 @@ extends AbstractContributionItem
 		}	
 	};	
 
+	protected Menu createMenu() 
+	{
+		Menu menu = new Menu(RCPUtil.getActiveWorkbenchShell(), SWT.POP_UP);
+		searchTypes = new ArrayList<String>(SearchResultProviderRegistry.sharedInstance().getRegisteredNames());
+		for (String type : searchTypes) {
+			MenuItem menuItem = new MenuItem(menu, SWT.PUSH);
+			menuItem.setText(type);
+			menuItem.addSelectionListener(new SelectionListener(){			
+				public void widgetSelected(SelectionEvent e) {
+					selectedType = ((MenuItem) e.getSource()).getText();
+					searchPressed();
+				}			
+				public void widgetDefaultSelected(SelectionEvent e) {
+					widgetSelected(e);
+				}			
+			});
+		}
+		selectedType = searchTypes.get(0);		
+		return menu;
+	}
+	
+	public void fill(ToolBar parent, int index) 
+	{
+		if (fillToolBar) 
+		{
+			toolItem = new ToolItem(parent, SWT.SEPARATOR, index);			
+	  	createText(parent);
+	  	toolItem.setControl(searchText);
+	  	toolItem.setWidth(100);			
+	  	
+			final ToolBar toolBar = parent;
+	  	final Menu menu = createMenu();
+	  	final ToolItem searchItem = new ToolItem(parent, SWT.DROP_DOWN);
+	  	searchItem.setImage(SharedImages.getSharedImage(NLBasePlugin.getDefault(), 
+					SearchContributionItem.class));
+	  	searchItem.addListener(SWT.Selection, new Listener(){	
+	  		public void handleEvent(Event event) {
+	  			if (event.detail == SWT.ARROW) {
+	  				Rectangle rect = searchItem.getBounds();
+	  				Point p = new Point(rect.x, rect.y + rect.height);
+	  				p = toolBar.toDisplay(p);
+	  				menu.setLocation(p.x, p.y);
+	  				menu.setVisible(true);
+	  			}
+	  			if (event.detail == SWT.NONE) {
+	  				searchPressed();
+	  			}
+	  		}
+	  	});
+	  	toolBar.layout(true, true);	 	  	
+		}
+	}	
+		
+	@Override
+	public void fill(CoolBar parent, int index) 
+	{
+		if (fillCoolBar) 
+		{
+			final CoolBar coolBar = parent;
+			final ToolBar toolBar = new ToolBar(coolBar, SWT.FLAT | SWT.WRAP);			
+	  	final Menu menu = createMenu();
+	  	
+	  	toolItem = new ToolItem(toolBar, SWT.SEPARATOR);			
+	  	createText(toolBar);
+	  	toolItem.setControl(searchText);
+	  	toolItem.setWidth(100);
+	  	
+	  	final ToolItem searchItem = new ToolItem(toolBar, SWT.DROP_DOWN);	  	
+	  	searchItem.setImage(SharedImages.getSharedImage(NLBasePlugin.getDefault(), 
+					SearchContributionItem.class));
+	  	searchItem.addListener(SWT.Selection, new Listener(){	
+	  		public void handleEvent(Event event) 
+	  		{
+	  			logger.info("event.detail = "+event.detail);
+	  			if (event.detail == SWT.ARROW) {
+	  				Rectangle rect = searchItem.getBounds();
+	  				Point p = new Point(rect.x, rect.y + rect.height);
+	  				p = toolBar.toDisplay(p);
+	  				menu.setLocation(p.x, p.y);
+	  				menu.setVisible(true);
+	  			}
+	  			if (event.detail == SWT.NONE) {
+	  				searchPressed();
+	  			}	  			
+	  		}
+	  	});	  	
+	  	toolBar.layout(true, true);
+	  	
+	  	CoolItem coolItem = new CoolItem(coolBar, SWT.SEPARATOR);
+	  	coolItem.setControl(toolBar);
+	  	Point size = toolBar.computeSize(SWT.DEFAULT, SWT.DEFAULT);	  	
+	  	Point coolSize = coolItem.computeSize(size.x, size.y);
+	  	coolItem.setMinimumSize(coolSize.x - 10, coolSize.y);
+//	  	coolItem.setPreferredSize(coolSize.x, coolSize.y);
+	  	coolItem.setSize(coolSize.x - 10, coolSize.y);	  	
+//	  	logger.info("size = "+size);
+		}
+	}
+		
 }
