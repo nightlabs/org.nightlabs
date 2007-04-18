076c339ea9bb780317c2786d04f2c86568d166cf
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-04-18 17:32:12 +0000

    removed unnecessary import
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@6213 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/base/language/LanguageChooserCombo.java
+++ src/org/nightlabs/base/language/LanguageChooserCombo.java
@@ -37,7 +37,6 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.base.custom.XCombo;
 import org.nightlabs.base.exceptionhandler.ExceptionHandlerRegistry;
