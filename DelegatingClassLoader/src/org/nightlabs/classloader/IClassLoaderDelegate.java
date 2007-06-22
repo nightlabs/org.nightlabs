4e39d4c71a64eb715c9dd29669a6d5a530af7241
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2007-06-22 16:28:30 +0000

    delete double stuff
    git-svn-id: https://svn.jfire.org/svn/main/trunk/DelegatingClassLoader@7427 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- DelegatingClassLoader/src/org/nightlabs/classloader/IClassLoaderDelegate.java
+++
@@ -1,7 +0,0 @@
-package org.nightlabs.classloader;
-
-import java.security.ProtectionDomain;
-
-public interface IClassLoaderDelegate {
-    public Class<?> delegateDefineClass(String name, byte[] b, int off, int len, ProtectionDomain protectionDomain);
-}
