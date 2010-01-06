0cc882e732389d93024d20e2eb0b53f239a20c32
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-01-06 01:09:26 +0000

    Bugfix: Added missing delegation methods in JDOQueryCollectionDecorator.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.query.jdo@16557 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jdo/query/JDOQueryCollectionDecorator.java
+++ src/org/nightlabs/jdo/query/JDOQueryCollectionDecorator.java
@@ -1,14 +1,16 @@
 package org.nightlabs.jdo.query;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import javax.jdo.PersistenceManager;
 
 /**
- * A Decorator for a QueryCollection that contains JDO-capable queries. This decorator sets the 
+ * A Decorator for a QueryCollection that contains JDO-capable queries. This decorator sets the
  * given PersitenceManager for all JDO-capable queries.
- * 
+ *
  * @author Marius Heinzmann - marius[at]nightlabs[dot]com
+ * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
  */
 public class JDOQueryCollectionDecorator<Q extends AbstractSearchQuery>
 	extends QueryCollection<Q>
@@ -25,7 +27,7 @@ public class JDOQueryCollectionDecorator<Q extends AbstractSearchQuery>
 		super(wrappedCollection.getResultClass());
 		this.wrappedCollection = wrappedCollection;
 	}
-	
+
 	@Override
 	public Collection<?> executeQueries()
 	{
@@ -56,4 +58,92 @@ public class JDOQueryCollectionDecorator<Q extends AbstractSearchQuery>
 		assert pm != null;
 		this.pm = pm;
 	}
+
+	@Override
+	public boolean addAll(Collection<? extends Q> c) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return wrappedCollection.contains(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return wrappedCollection.containsAll(c);
+	}
+
+	@Override
+	public long getFromInclude() {
+		return wrappedCollection.getFromInclude();
+	}
+
+	@Override
+	public long getToExclude() {
+		return wrappedCollection.getToExclude();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return wrappedCollection.isEmpty();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator<Q> iterator() {
+		return (Iterator<Q>) wrappedCollection.iterator();
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public void setFromInclude(long fromInclude) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public void setResultClass(Class<?> resultClass) {
+		if (getResultClass() != null) // this method is called by the constructor, hence we need to allow this first call.
+			throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public void setToExclude(long toExclude) {
+		throw new UnsupportedOperationException("The JDOQueryCollectionDecorator is read-only!");
+	}
+
+	@Override
+	public int size() {
+		return wrappedCollection.size();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return wrappedCollection.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return wrappedCollection.toArray(a);
+	}
+
 }
