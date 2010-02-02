f22fea77492209153bd8da08469725b65ed5e2d7
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-02-02 05:18:41 +0000

    1. Changed the wrong parameter (from File --> byte[]). 2. Added the getBytesFromFile method into the IOUtil.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@16794 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/util/IOUtil.java
+++ src/org/nightlabs/util/IOUtil.java
@@ -1752,4 +1752,29 @@ public class IOUtil
 				writer.write(stringToWrite);
 		} // while (stk.nextToken() != StreamTokenizer.TT_EOF) {
 	}
+	
+	public static byte[] getBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+    
+        // Get the size of the file
+        long length = file.length();
+    
+        byte[] bytes = new byte[(int)length];
+    
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+    
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file "+file.getName());
+        }
+    
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+    }
 }
