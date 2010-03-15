874d18d568aa784fcffaa656f9273e093b6ce226
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-03-15 16:39:37 +0000

    Bugfix: Wrong package name in String constant.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.query.jdo@17114 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jdo/query/resource/Messages.java
+++ src/org/nightlabs/jdo/query/resource/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-	private static final String BUNDLE_NAME = "org.nightlabs.jdo.resource.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.nightlabs.jdo.query.resource.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle(BUNDLE_NAME);
