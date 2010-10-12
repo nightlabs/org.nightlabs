22b0e621a4a44a8069189a534cabc1f06a5fbfac
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2010-10-12 21:46:56 +0000

    * Refactored some constructors to improve readability and programmer-friendlyness. * Introduced new CTETariff as a subclass of Tariff.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@18096 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/l10n/Currency.java
+++ src/org/nightlabs/l10n/Currency.java
@@ -23,23 +23,26 @@
  **********************************************************************/
 package org.nightlabs.l10n;
 
+import java.math.BigDecimal;
+
 /**
  * @author Marco Schulze - marco at nightlabs dot de
  */
 public interface Currency
 {
 	/**
-	 * @return Returns the international currency id (e.g. USD, EUR).
+	 * Get the ISO currency ID.
+	 * @return the international currency id (e.g. USD, EUR).
 	 */
 	String getCurrencyID();
-	
+
 	/**
-	 * We manage currency values as integers (int or long) and therefore
+	 * We manage currency values as integers (usually of type <code>long</code>) and therefore
 	 * all values have to be divided (usually by 100) before being displayed.
-	 * And after parsing, the entered amount needs to be multiplied.
+	 * After parsing, the entered amount needs to be multiplied.
 	 * The divisor/multiplicator is 10^decimalDigitCount.
-	 * 
-	 * @return Returns the number of digits after the decimal separator.
+	 *
+	 * @return the number of digits after the decimal separator.
 	 */
 	int getDecimalDigitCount();
 
@@ -47,7 +50,66 @@ public interface Currency
 	 * Instead of displaying the international currency id, you might want to
 	 * display the real currency symbol.
 	 *
-	 * @return Returns the currency symbol (e.g. $).
+	 * @return the currency symbol (e.g. € or $).
 	 */
 	String getCurrencySymbol();
+
+	/**
+	 * <p>
+	 * Returns the given amount in the double value of this currency.
+	 * </p>
+	 * <p>
+	 *   amount / 10^(decimalDigitCount)
+	 * <p>
+	 *
+	 * @param amount the amount to be converted.
+	 * @return the approximate value as double - there might be rounding differences.
+	 */
+	double toDouble(long amount);
+
+	/**
+	 * <p>
+	 * Convert the given amount to the long value of this currency.
+	 * </p>
+	 * <p>
+	 *   amount * 10^(decimalDigitCount)
+	 * <p>
+	 *
+	 * @param amount the amount to be converted.
+	 * @return the approximate value as long - there might be rounding differences.
+	 */
+	long toLong(final double amount);
+
+	/**
+	 * <p>
+	 * Convert the given amount to a {@link BigDecimal}.
+	 * </p>
+	 * <p>
+	 * This method
+	 * behaves like {@link #toDouble(long)} but without loosing
+	 * information as a <code>BigDecimal</code> can be precise.
+	 * </p>
+	 * <p>
+	 * The {@link BigDecimal#scale() result's scale} will be the same
+	 * as the {@link #getDecimalDigitCount() decimalDigitCount} of this currency.
+	 * </p>
+	 *
+	 * @param amount the amount to be converted (in the currency's sub-unit).
+	 * @return floating-point value in the currency (e.g. 20.56 for the long value 2056 and the currency € [having a decimal digit count of 2]).
+	 */
+	BigDecimal toBigDecimal(long amount);
+
+	/**
+	 * <p>
+	 * Convert the given amount from a {@link BigDecimal} to a <code>long</code>.
+	 * </p>
+	 * <p>
+	 * This method behaves like {@link #toLong(double)}.
+	 * </p>
+	 *
+	 * @param amount the amount to be converted.
+	 * @return the approximate value as long (there might be rounding differences) in the currency's sub-unit (e.g. in Cent when using €).
+	 */
+	long toLong(BigDecimal amount);
+
 }
