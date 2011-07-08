e0bdda29cae3e7c6c1cda6519f387687154a7e2d
marc <marc@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-07-08 14:57:39 +0000

    query parameters
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jdo@19014 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/test/java/org/nightlabs/jdo/moduleregistry/UpdateHistoryItemTest.java
+++ src/test/java/org/nightlabs/jdo/moduleregistry/UpdateHistoryItemTest.java
@@ -1,9 +1,20 @@
 package org.nightlabs.jdo.moduleregistry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.nightlabs.test.jdo.PersistenceCapableTestBase;
 
 public class UpdateHistoryItemTest extends PersistenceCapableTestBase<UpdateHistoryItem> {
 	public UpdateHistoryItemTest() {
 		super(UpdateHistoryItem.class);
+	}
+	
+	@Override
+	protected Map<String, Object> getNamedQueryParameters(String queryName) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("moduleID", "MODULE ID");
+		result.put("updateHistoryItemID", "UPDATE HISTORY ITEM ID");
+		return result;
 	}
 }
