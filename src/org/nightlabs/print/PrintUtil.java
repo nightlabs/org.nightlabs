03c753dff3f6d2d77d8606706cbd9e1a1113c4bf
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-08-19 12:18:51 +0000

    * workaround for PDF printing bug concerning Samsung printer (letter format)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.base@19236 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/print/PrintUtil.java
+++ src/org/nightlabs/print/PrintUtil.java
@@ -29,13 +29,15 @@ import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
 
 /**
  * Utils for accessing printers.
- * 
+ *
  * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de -->
  */
 public class PrintUtil {
@@ -46,24 +48,24 @@ public class PrintUtil {
 //				printerUseCaseID
 //			);
 //	}
-	
-	public static PrinterInterface getDocumentPrinterInterface(String printerUseCaseID) {
+
+	public static PrinterInterface getDocumentPrinterInterface(final String printerUseCaseID) {
 		// TODO: Implement platform dependent PrinterInterface for document printing.
 		return null;
 	}
-	
+
 	/**
 	 * Assigns the given printerJob to a PrintService. If the default printer is to be used
 	 * this method tries to lookup the default print service and assign it. If no default is
-	 * 
+	 *
 	 * @param printerJob
 	 * @param useDefault
 	 * @return <code>true</code> if the user does not cancel the print dialog;
 	 * 		<code>false</code> otherwise.
 	 */
-	public boolean assignPrinterJobToService(PrinterJob printerJob, boolean useDefault) {
+	public boolean assignPrinterJobToService(final PrinterJob printerJob, final boolean useDefault) {
 		if (useDefault) {
-			PrintService defService = PrintServiceLookup.lookupDefaultPrintService();
+			final PrintService defService = PrintServiceLookup.lookupDefaultPrintService();
 			if (defService != null) {
 //				Printer
 //				printerJob.
@@ -71,67 +73,72 @@ public class PrintUtil {
 				try {
 //					printerJob.printDialog(attributes)
 					printerJob.setPrintService(defService);
-				} catch (PrinterException e) {
+				} catch (final PrinterException e) {
 					return false;
 				}
 				return true;
-			} else
+			} else {
 				return printerJob.printDialog();
+			}
 		}
 		else {
 			return printerJob.printDialog();
 		}
 	}
-	
-	
+
+
 	/**
 	 * Tries to lookup the printService with the given name.
 	 * Will return null if no such service is available.
-	 * 
+	 *
 	 * @param name The name of the {@link PrintService} to lookpu
 	 * @return A {@link PrintService} or null.
 	 */
-	public static PrintService lookupPrintService(String name) {
-		PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
-		for (PrintService element : services) {
-			if (element.getName().equals(name))
+	public static PrintService lookupPrintService(final String name) {
+		final PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
+		for (final PrintService element : services) {
+			if (element.getName().equals(name)) {
 				return element;
+			}
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Returns a {@link List} of all {@link PrintService}s found.
-	 * 
+	 *
 	 * @return A {@link List} of all {@link PrintService}s found.
 	 */
 	public static List<PrintService> lookupPrintServices() {
-		PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
-		ArrayList<PrintService> result = new ArrayList<PrintService>();
-		for (PrintService element : services) {
+		final PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
+		final ArrayList<PrintService> result = new ArrayList<PrintService>();
+		for (final PrintService element : services) {
 			result.add(element);
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Returns the {@link PrintService} configured to the settings of the given printerConfiguration.
-	 * 
+	 *
 	 * @param printerConfiguration The printerConfiguration to apply.
 	 * @param throwExceptionIfNotFound Whether to throw an exception if the printservice specified in the given printConfiguration could not be found and also no default is assigned.
 	 * @return The {@link PrintService} configured to the settings of the given printerConfiguration.
 	 * @throws PrinterException Thrown depending on the throwExceptionIfNotFound parameter.
 	 */
-	public static PrintService getConfiguredPrintService(PrinterConfiguration printerConfiguration, boolean throwExceptionIfNotFound)
+	public static PrintService getConfiguredPrintService(final PrinterConfiguration printerConfiguration, final boolean throwExceptionIfNotFound)
 	throws PrinterException
 	{
 		PrintService printService = null;
-		if (printerConfiguration != null && printerConfiguration.getPrintServiceName() != null)
+		if (printerConfiguration != null && printerConfiguration.getPrintServiceName() != null) {
 			printService = lookupPrintService(printerConfiguration.getPrintServiceName());
-		if (printService == null)
+		}
+		if (printService == null) {
 			printService = PrintServiceLookup.lookupDefaultPrintService();
-		if (printService == null && throwExceptionIfNotFound)
+		}
+		if (printService == null && throwExceptionIfNotFound) {
 			throw new PrinterException("Could not find a configured or default print service");
+		}
 		// TODO: Still have to configure the print-service
 		return printService;
 	}
@@ -140,69 +147,77 @@ public class PrintUtil {
 	 * Creates a new PrinterJob and assigns it the PrintService in the given
 	 * PrinterConfiguration. The PrintService will be configured to the settings
 	 * of the given printerConfiguration.
-	 * 
+	 *
 	 * @param printerConfiguration The printerConfiguration to apply.
 	 * @return A new PrinterJob ready configured for the given PrinterConfiguration.
 	 * @throws PrinterException If the PrintService in the given PrinterConfiguration could not be found.
 	 */
-	public static PrinterJob getConfiguredPrinterJob(PrinterConfiguration printerConfiguration) throws PrinterException {
+	public static PrinterJob getConfiguredPrinterJob(final PrinterConfiguration printerConfiguration) throws PrinterException {
 		// Creates and returns a PrinterJob which is initially associated with the default printer.
-	    PrinterJob printerJob = PrinterJob.getPrinterJob();
+	    final PrinterJob printerJob = PrinterJob.getPrinterJob();
 
 	    // Returns a PrintService describing the capabilities of the printer given by the printerConfiguration.
 	    // The PrintService is also configured in compliance with the settings of this configuration.
-		PrintService printService = PrintUtil.getConfiguredPrintService(printerConfiguration, true);
+		final PrintService printService = PrintUtil.getConfiguredPrintService(printerConfiguration, true);
 
 		// Associates the PrinterJob with the PrintService.
 		printerJob.setPrintService(printService);
 
 		return printerJob;
 	}
-	
+
 	/**
 	 * Retuns the default(current) {@link PageFormat} for the given {@link PrintService}.
-	 * 
+	 *
 	 * @param printService The {@link PrintService} to get the {@link PageFormat} of.
 	 * @return The default(current) {@link PageFormat} for the given {@link PrintService}.
 	 */
-	public static PageFormat getDefaultPageFormat(PrintService printService)
+	public static PageFormat getDefaultPageFormat(final PrintService printService)
 	throws PrinterException
 	{
-		PrinterJob printerJob = PrinterJob.getPrinterJob();
+		final PrinterJob printerJob = PrinterJob.getPrinterJob();
 		printerJob.setPrintService(printService);
 		return printerJob.defaultPage();
 	}
-	
+
 	/**
 	 * Returns whether the given {@link PageFormat}s equal regarding their paper.
 	 * It will size, imageable position and imageable size of the given formats.
-	 * 
+	 *
 	 * @param pageFormat1 The first {@link PageFormat} to compare.
 	 * @param pageFormat2 The second {@link PageFormat} to compare.
 	 */
-	public static boolean pageFormatEquals(PageFormat pageFormat1, PageFormat pageFormat2) {
-		if (pageFormat1 == null && pageFormat2 == null)
+	public static boolean pageFormatEquals(final PageFormat pageFormat1, final PageFormat pageFormat2) {
+		if (pageFormat1 == null && pageFormat2 == null) {
 			return true;
-		if (pageFormat1 == null && pageFormat2 != null)
+		}
+		if (pageFormat1 == null && pageFormat2 != null) {
 			return false;
-		if (pageFormat2 == null && pageFormat1 != null)
+		}
+		if (pageFormat2 == null && pageFormat1 != null) {
 			return false;
+		}
 		boolean result = false;
 		result = pageFormat1.getHeight() == pageFormat2.getHeight();
-		if (!result)
+		if (!result) {
 			return result;
+		}
 		result = pageFormat1.getWidth() == pageFormat2.getWidth();
-		if (!result)
+		if (!result) {
 			return result;
+		}
 		result = pageFormat1.getImageableX() == pageFormat2.getImageableX();
-		if (!result)
+		if (!result) {
 			return result;
+		}
 		result = pageFormat1.getImageableY() == pageFormat2.getImageableY();
-		if (!result)
+		if (!result) {
 			return result;
+		}
 		result = pageFormat1.getImageableHeight() == pageFormat2.getImageableHeight();
-		if (!result)
+		if (!result) {
 			return result;
+		}
 		result = pageFormat1.getImageableWidth() == pageFormat2.getImageableWidth();
 		return result;
 	}
@@ -211,21 +226,41 @@ public class PrintUtil {
 	 * Sets the given {@link Printable} to the given {@link PrinterJob}. It will
 	 * check whether the {@link PageFormat} assigned to the job equals the given pageFormat
 	 * and set the printable with the pageFormat only if they differ. This is likely to save some time.
-	 * 
+	 *
 	 * @param printJob The {@link PrinterJob} to set the printable.
 	 * @param printable The {@link Printable} to set.
 	 * @param pageFormat The {@link PageFormat} to set to the job.
 	 */
-	public static void setPrinterJobPrintable(PrinterJob printJob, Printable printable, PageFormat pageFormat) {
-		if (pageFormat == null)
+	public static void setPrinterJobPrintable(final PrinterJob printJob, final Printable printable, final PageFormat pageFormat) {
+		if (pageFormat == null) {
 			printJob.setPrintable(printable);
-		else {
-			PageFormat jobFormat = printJob.defaultPage();
-			if (pageFormatEquals(pageFormat, jobFormat))
+		} else {
+			final PageFormat jobFormat = printJob.defaultPage();
+			if (pageFormatEquals(pageFormat, jobFormat)) {
 				printJob.setPrintable(printable);
-			else
+			} else {
 				printJob.setPrintable(printable, pageFormat);
+			}
 		}
-			
+	}
+
+	/**
+	 * Returns the printer configuration for the given use case ID or null if no such configuration could be found.
+	 * @param useCaseID The use case ID to search the configuration for.
+	 * @return the printer configuration or null
+	 */
+	public static PrinterConfiguration getPrinterConfigurationFor(final String useCaseID) {
+		PrinterConfiguration printerConfig = null;
+
+		final Map<String, PrinterConfiguration> configs = PrinterConfigurationCfMod.getPrinterConfigurationCfMod()
+			.getPrinterConfigurations();
+		for (final Entry<String, PrinterConfiguration> entry : configs.entrySet()) {
+			if (entry.getKey().equals(useCaseID)) {
+				printerConfig = (PrinterConfiguration) entry.getValue().clone();
+				break;
+			}
+		}
+
+		return printerConfig;
 	}
 }
