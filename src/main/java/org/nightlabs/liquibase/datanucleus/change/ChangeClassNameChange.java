54a2c2bfddf0baf533581420e25ccbefd184db24
marius <marius@b9467f74-ba07-0410-ab41-ffa09155a584>
2011-12-16 13:12:03 +0000

    - added the discriminatorColumn, oldDiscriminatorValue and newDiscriminatorValue as configuration fields.  - added new update logic to migrate the discriminator values  - restructured the class to make it better readable.
    git-svn-id: https://svn.nightlabs.org/svn/main/trunk/org.nightlabs.liquibase.datanucleus@2616 b9467f74-ba07-0410-ab41-ffa09155a584

--- src/main/java/org/nightlabs/liquibase/datanucleus/change/ChangeClassNameChange.java
+++ src/main/java/org/nightlabs/liquibase/datanucleus/change/ChangeClassNameChange.java
@@ -1,13 +1,13 @@
-/**
- * 
- */
 package org.nightlabs.liquibase.datanucleus.change;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import liquibase.change.ChangeMetaData;
 import liquibase.database.Database;
+import liquibase.exception.DatabaseException;
 import liquibase.exception.SetupException;
 import liquibase.logging.LogFactory;
 import liquibase.logging.Logger;
@@ -16,13 +16,13 @@ import liquibase.statement.core.RenameTableStatement;
 import liquibase.statement.core.UpdateStatement;
 
 import org.nightlabs.liquibase.datanucleus.util.DNUtil;
-import org.nightlabs.liquibase.datanucleus.util.DNUtil.NucleusTablesStruct;
 import org.nightlabs.liquibase.datanucleus.util.LiquibaseUtil;
+import org.nightlabs.liquibase.datanucleus.util.DNUtil.NucleusTablesStruct;
 
 /**
- * A Change that changes the className of a persistent class in the
- * datanucleus-indes nucleus-tables. This Change can optionally rename the table
- * data of the of the old class was stored in to the new table-name.
+ * A change that changes the className of a persistent class in the
+ * datanucleus-index nucleus-tables. This change can optionally rename the table
+ * where the old class was stored in.
  * <p>
  * Note that this can also be used to change the name and table of a
  * collection-type field inside a class (that is stored in an own table, for
@@ -37,6 +37,11 @@ import org.nightlabs.liquibase.datanucleus.util.LiquibaseUtil;
  * </li>
  * <li>newClassName: The fully qualified <b>new</b> name it should to be changed
  * to</li>
+ * <li>discriminatorColumn: The optional discriminator column name; defaults to 'discriminator'.</li>
+ * <li>oldDiscriminatorValue: The optional old discriminator value which will be replaced with the new one. 
+ * 	If none is given, the class name strategy is assumed and the className is taken.</li>
+ * <li>newDiscriminatorValue: The optional new discriminator value which will be replacing the old one. 
+ * 	If none is given, the class name strategy is assumed and the newClassName is taken.</li>
  * <li>renameTable: Optional parameter, defaults to <code>true</code>. Defines
  * whether the table the data for the old class (or field) was stored in should
  * be renamed to. If set to true, the 'newTableName' attribute has to be set,
@@ -50,11 +55,16 @@ import org.nightlabs.liquibase.datanucleus.util.LiquibaseUtil;
  */
 public class ChangeClassNameChange extends AbstractDNChange {
 
+	private static final String DISCRIMINATOR_COLUMN_DEFAULT = "discriminator";
+
 	private static final Logger logger = LogFactory.getLogger();
 	
 	private String className;
 	private String newClassName;
 	private String newTableName;
+	private String discriminatorColumn = DISCRIMINATOR_COLUMN_DEFAULT;
+	private String oldDiscriminatorValue;
+	private String newDiscriminatorValue;
 	private boolean renameTable = true;
 	private boolean dropConstraints = true;
 	
@@ -70,6 +80,19 @@ public class ChangeClassNameChange extends AbstractDNChange {
 		}
 		if (null == getNewClassName() || getNewClassName().isEmpty()) {
 			throw new SetupException(getChangeMetaData().getName() + " requires the 'newClassName'-attribute to be set.");
+		}
+		
+		// Assumes the classname as default discriminator strategy.
+		if (oldDiscriminatorValue == null && newDiscriminatorValue == null)
+		{
+			oldDiscriminatorValue = className;
+			newDiscriminatorValue = newClassName;
+		}
+		else if (oldDiscriminatorValue != null && newDiscriminatorValue == null || 
+				oldDiscriminatorValue == null && newDiscriminatorValue != null)
+		{
+			throw new SetupException(getChangeMetaData().getName() + " requires that either no {old,new}DiscriminatorValue " +
+					"is set or both have to be set!");
 		}
 	}
 
@@ -88,11 +111,24 @@ public class ChangeClassNameChange extends AbstractDNChange {
 		if (null == oldTableName || oldTableName.isEmpty()) {
 			logger.severe(getChangeMetaData().getName() + " can't find the oldTableName for class " + getClassName() + ", aborting");
 			return statements;
-//			throw new RuntimeException(getChangeMetaData().getName() + " can't find the oldTableName, aborting.");
 		}		
 		
-		// For the class-entry itself but as well as for all collection-type fields we update the nucleus tables. 
-		for (NucleusTablesStruct struct : DNUtil.getNucleusReferences(getDb(), getClassName())) {
+		statements.addAll(updateDNClassInformation(oldTableName));
+		statements.addAll(updateDiscriminatorColumn(oldTableName));
+		
+		if (isTableNameUpdateNeeded(oldTableName)) {
+			statements.addAll(updateTableName(oldTableName));
+		}
+		return statements;
+	}
+
+	private List<SqlStatement> updateDNClassInformation(String oldTableName)
+	{
+		// For the class-entry itself but as well as for all collection-type fields we update the nucleus tables.
+		Collection<NucleusTablesStruct> nucleusReferences = DNUtil.getNucleusReferences(getDb(), getClassName());
+		List<SqlStatement> statements = new ArrayList<SqlStatement>(nucleusReferences.size() + 1);
+		
+		for (NucleusTablesStruct struct : nucleusReferences) {
 			String newClassValue = getNewClassName() + struct.getClassName().replace(getClassName(), "");
 			// Update nucleus_tables to reference the new class-name
 			UpdateStatement update = new UpdateStatement(getSchemaName(), DNUtil.getNucleusTablesName());
@@ -104,26 +140,92 @@ public class ChangeClassNameChange extends AbstractDNChange {
 		
 		// Update nucleus_tables to reference the new class-name
 		UpdateStatement update = new UpdateStatement(getSchemaName(), DNUtil.getNucleusTablesName());
-		update.setWhereClause(DNUtil.getNucleusTableNameColumn() + " = '" + database.escapeStringForDatabase(oldTableName) + "'");
+		update.setWhereClause(DNUtil.getNucleusTableNameColumn() + " = ?");
+		update.addWhereParameter(oldTableName);
 		update.addNewColumnValue(DNUtil.getNucleusClassNameColumn(), getNewClassName());
 		statements.add(update);
 		
-		if (null != getNewTableName() && !getNewTableName().isEmpty()) {
-			// The table name has changed also, re-reference and rename the table
-			update = new UpdateStatement(getSchemaName(), DNUtil.getNucleusTablesName());
-			update.setWhereClause(DNUtil.getNucleusClassNameColumn() + " = ?");
-			update.addWhereParameter(getNewClassName());
-			update.addNewColumnValue(DNUtil.getNucleusTableNameColumn(), getNewTableName());
-			statements.add(update);
-			
-			if (isRenameTable()) {
-				statements.add(new RenameTableStatement(getSchemaName(), oldTableName, getNewTableName()));
-				if (isDropConstraints()) {
-					LiquibaseUtil.addDropConstraintsStatements(getDb(), getChangeSet(), statements, getSchemaName(), getNewTableName());
-				}
+		return statements;
+	}
+	
+	private List<SqlStatement> updateDiscriminatorColumn(String oldTableName)
+	{
+		try {
+			return doUpdateDiscriminatorColumn(oldTableName);
+		} catch (Exception e) {
+			if (e instanceof RuntimeException) {
+				throw (RuntimeException) e;
+			}
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Update the discriminator column if existing.
+	 * <p>                                                                   
+	 * This method assumes two things:
+	 * 	<ol>                                                                               
+	 *   <li>The discriminator column is always existing under the default name 'discriminator'</li>                       
+	 *   <li>The discrimator strategy is classname OR if another strategy is used, that strategy does NEVER enter the 
+	 *       classname (the old classname). <br/>
+	 *       Otherwise the name is now replaced with the new classname altough the discriminator is not correct. </li>
+	 *  </ol>       
+	 * </p>
+	 * @param statements
+	 * @param oldTableName
+	 * @throws DatabaseException
+	 * @throws SQLException
+	 */
+	private List<SqlStatement> doUpdateDiscriminatorColumn(String oldTableName)
+		throws DatabaseException, SQLException
+	{
+		List<SqlStatement> statements = new ArrayList<SqlStatement>(1);
+		Collection<String> columnNames = getColumnNames(oldTableName);
+		if (columnNames.contains(getDiscriminatorColumn())) {
+			// always assumes that the default discriminator column name is used and no other. 
+			UpdateStatement discriminatorUpdate = new UpdateStatement(getSchemaName(), DNUtil.getNucleusTablesName());
+			discriminatorUpdate.setWhereClause(getDiscriminatorColumn() + " = ?");
+			discriminatorUpdate.addWhereParameter(getOldDiscriminatorValue());
+			discriminatorUpdate.addNewColumnValue(getDiscriminatorColumn(), getNewDiscriminatorValue());
+			statements.add(discriminatorUpdate);
+		}
+		return statements;
+	}
+
+	/**
+	 * Returns true if a new table name is set and it differs from the old one.
+	 * 
+	 * @param oldTableName The old table name.
+	 * @return true if a new table name is set and it differs from the old one.
+	 */
+	private boolean isTableNameUpdateNeeded(String oldTableName)
+	{
+		return null != getNewTableName() && !getNewTableName().isEmpty() && !getNewClassName().equals(oldTableName);
+	}
+	
+	/**
+	 * Creates update statements to update the Tablename for the given class in datanucleus management tables and 
+	 * of the class itself if {@link #renameTable} is set.
+	 * 
+	 * @param oldTableName The old table name.
+	 * @return the update statements changing the table name.
+	 */
+	private List<SqlStatement> updateTableName(String oldTableName)
+	{
+		List<SqlStatement> statements = new ArrayList<SqlStatement>(2);
+		// The table name has changed also, re-reference and rename the table
+		UpdateStatement update = new UpdateStatement(getSchemaName(), DNUtil.getNucleusTablesName());
+		update.setWhereClause(DNUtil.getNucleusClassNameColumn() + " = ?");
+		update.addWhereParameter(getNewClassName());
+		update.addNewColumnValue(DNUtil.getNucleusTableNameColumn(), getNewTableName());
+		statements.add(update);
+		
+		if (isRenameTable()) {
+			statements.add(new RenameTableStatement(getSchemaName(), oldTableName, getNewTableName()));
+			if (isDropConstraints()) {
+				LiquibaseUtil.addDropConstraintsStatements(getDb(), getChangeSet(), statements, getSchemaName(), getNewTableName());
 			}
 		}
-		
 		return statements;
 	}
 	
@@ -141,6 +243,54 @@ public class ChangeClassNameChange extends AbstractDNChange {
 
 	public void setNewTableName(String newTableName) {
 		this.newTableName = newTableName;
+	}
+
+	/**
+	 * @return the discriminatorColumn
+	 */
+	public String getDiscriminatorColumn()
+	{
+		return discriminatorColumn;
+	}
+
+	/**
+	 * @param discriminatorColumn the discriminatorColumn to set
+	 */
+	public void setDiscriminatorColumn(String discriminatorColumn)
+	{
+		this.discriminatorColumn = discriminatorColumn;
+	}
+
+	/**
+	 * @return the oldDiscriminatorValue
+	 */
+	public String getOldDiscriminatorValue()
+	{
+		return oldDiscriminatorValue;
+	}
+
+	/**
+	 * @param oldDiscriminatorValue the oldDiscriminatorValue to set
+	 */
+	public void setOldDiscriminatorValue(String oldDiscriminatorValue)
+	{
+		this.oldDiscriminatorValue = oldDiscriminatorValue;
+	}
+
+	/**
+	 * @return the newDiscriminatorValue
+	 */
+	public String getNewDiscriminatorValue()
+	{
+		return newDiscriminatorValue;
+	}
+
+	/**
+	 * @param newDiscriminatorValue the newDiscriminatorValue to set
+	 */
+	public void setNewDiscriminatorValue(String newDiscriminatorValue)
+	{
+		this.newDiscriminatorValue = newDiscriminatorValue;
 	}
 
 	public boolean isRenameTable() {
